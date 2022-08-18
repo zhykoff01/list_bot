683bff6049a1fa85f54f922cafbc5993fecc34a4
zhyk01 <zhykoff01@gmail.com>
2022-08-18 15:20:44 +0400

    Много всякого наделал, надо было сразу гит поставить

--- parser/main.py
+++ parser/main.py
@@ -16,31 +16,27 @@ def get_soup():
 result_list = {'href': [], 'title': [], 'cost': [], 'about': [], 'category': []}
 
 
-def get_top_item():
-    first_div_dl = get_soup().find('div', class_='dl')
-    all_href_a = first_div_dl.find_all('a')
-
-    for item in all_href_a:
-        result_list['href'].append('https://www.list.am/ru' + item.get('href'))
-        result_list['title'].append(item.find_next('div').find('div').get_text())
-        result_list['cost'].append(item.find_next('div', class_='p').get_text())
-        result_list['about'].append(item.find_next('div', class_='at').get_text())
-        result_list['category'].append(item.find_next('div', class_='c').get_text())
-    return result_list
-
-
 def get_item():
+    first_div_dl = get_soup().find('div', class_='dl')
+    all_first_href_a = first_div_dl.find_all('a')
     second_div_dl = get_soup().find('div', class_='dl').find_next('div', class_='dl')
-    all_href_a = second_div_dl.find_all('a')
+    all_second_href_a = second_div_dl.find_all('a')
+    all_second_href_a = all_second_href_a[:-7]
+    all_href_a = all_first_href_a + all_second_href_a
 
     for item in all_href_a:
         result_list['href'].append('https://www.list.am/ru' + item.get('href'))
         result_list['title'].append(item.find_next('div').find('div').get_text())
-        result_list['cost'].append(item.find_next('div', class_='p').get_text())
-        result_list['about'].append(item.find_next('div', class_='at').get_text())
+        if item.find_next('div', class_='p') is not None:
+            result_list['cost'].append(item.find_next('div', class_='p').get_text())
+        else:
+            result_list['cost'].append('')
+        if item.find_next('div', class_='at') is not None:
+            result_list['about'].append(item.find_next('div', class_='at').get_text())
+        else:
+            result_list['about'].append('')
         result_list['category'].append(item.find_next('div', class_='c').get_text())
     return result_list
 
 
-# print(get_top_item())
 print(get_item())
